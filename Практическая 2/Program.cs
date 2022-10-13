c42859b4326b45af12e1c25a72689daf3bfb05d4
dimap <dimap@DESKTOP-4IVPGHB>
2022-10-13 21:35:36 +0300

    Добавьте файлы проекта.

---
+++ Практическая 2/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace Две_Игры
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            do
+            {
+                Console.WriteLine("Выберите программу: 1. Игра Угадай число;  2. Таблица умножения;   3. Выход");
+                string chislo = Console.ReadLine();
+                int b = Convert.ToInt32(chislo);
+
+                if (b == 1)
+                {
+                    Ugadai_chislo();
+                }
+
+                else if (b == 2)
+                {
+                    Tablica();
+                }
+
+                else if (b == 3)
+                {
+                    Console.WriteLine("Завершение программы");
+                    a = false;
+                }
+
+            } while (a == true);
+        }
+        static void Ugadai_chislo()
+        {
+
+            bool n = true;
+            Random chislo = new Random();
+            int l = chislo.Next(0, 100);
+            do
+            {
+                Console.WriteLine("Введите предполагаемое число");
+                string k = Console.ReadLine();
+                int a = Convert.ToInt32(k);
+                Console.Clear();
+
+                Console.WriteLine("Для подсказки нажмите F1");
+                ConsoleKeyInfo g = Console.ReadKey();
+                if (g.Key == ConsoleKey.F1)
+                {
+                    int c = l / 2;
+                    if (c == 1)
+                    {
+                        Console.WriteLine("Число нечётное");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Число чётное");
+                    }
+
+
+                }
+                if (a > l)
+                {
+                    Console.WriteLine("Число меньше");
+                }
+                else if (a < l)
+                {
+                    Console.WriteLine("Число больше");
+                }
+                else if (a == l)
+                {
+                    Console.WriteLine("Вы победили");
+                    n = false;
+                }
+                
+            } while (n == true);
+
+
+
+
+        }
+        static void Tablica()
+        {
+
+            int[,] tabl = new int[10, 9];
+            for (int i = 1; i < 10; i++)
+            {
+                for (int v = 1; v < 9; v++)
+            {
+                    tabl[i, v] = i * v;
+                    Console.WriteLine(tabl[i, v] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
